0cac5f134dbcac862c5e742662a1eea88abc9bbd
bapportin <bastian@apportin.de>
2025-08-02 08:13:29 +0200

    bla

---
+++ data.py
@@ -0,0 +1,2 @@
+from stories import *
+from questions import *
---
+++ questions.py
@@ -0,0 +1,13 @@
+QUESTIONS=[
+    "What kind of person do you think is most likely to succeed in a difficult situation?",
+    "If someone has a secret, should they always tell it? Why or why not?",
+    "Do you think it’s better to be lucky or smart?",
+    "What kinds of help are most valuable — magical, practical, or social?",
+    "Is silence ever more powerful than speaking?",
+    "How should someone deal with people in power who are being unfair?",
+    "If you could have one unusual skill, what would it be and why?",
+    "What makes someone truly trustworthy?",
+    "Do you believe people can change their fate? Why or why not?",
+    "Can disobedience ever be a good thing?"
+
+    ]
